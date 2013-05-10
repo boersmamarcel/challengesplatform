8d1ea2f511257f09cb0cfbd611a50e42167dfaff
Marcel Boersma <boersma.marcel@gmail.com>
2013-05-10 19:01:23 +0200

    Adds files to social branch

---
+++ app/controllers/follow_controller.rb
@@ -0,0 +1,41 @@
+require 'pp'
+class FollowController < ApplicationController
+
+  def index
+    @following = current_user.following
+
+  end
+
+
+
+  def create
+    @followee = User.find(params[:user_id])
+    @follow = current_user.followrelations.build(:following_id => @followee.id)
+    respond_to do |format|
+      if @follow.save
+          format.html {redirect_to profile_user_path(@followee), notice: 'You are now following this user'}
+      else
+          format.html { redirect_to profile_user_path(@followee), notice: 'Whoops something went wrong'}
+      end
+
+    end
+
+  end
+
+
+
+  def destroy
+     @followee = User.find(params[:user_id])
+     @followrelation = Follow.where(:user_id => current_user.id, :following_id => @followee.id).first
+      
+      @followrelation.destroy
+
+      respond_to do |format|
+        format.html {redirect_to profile_user_path(@followee), notice: 'You stopped following this user'}
+        format.json { head :no_content}
+      end
+
+  end
+
+
+end
---
+++ test/unit/helpers/follow_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class FollowHelperTest < ActionView::TestCase
+end
