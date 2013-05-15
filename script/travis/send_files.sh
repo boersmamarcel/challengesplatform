94f8115cc73bd8c1b3f33f84d3b22388cf53fce1
Bas Veeling <basveeling@gmail.com>
2013-05-15 15:25:38 +0200

    fixed environmental variables expect

--- script/travis/send_files.sh
+++ script/travis/send_files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 set file [lindex $argv 0]
-spawn scp $file travis@$SECRET_BUNDLE_IP:~/www
+spawn scp $file travis@$env(SECRET_BUNDLE_IP):~/www
 expect "password:"
-send $SECRET_BUNDLE_PASS
+send $env(SECRET_BUNDLE_PASS)
 send "\r"
 expect eof
