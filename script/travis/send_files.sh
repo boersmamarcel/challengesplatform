f62b821b63b80fa10a39342c911cf5981fbc3d80
Bas Veeling <basveeling@gmail.com>
2013-05-15 15:43:36 +0200

    updated send files script

--- script/travis/send_files.sh
+++ script/travis/send_files.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/expect -f
+set timeout -1
 set file [lindex $argv 0]
-spawn scp $file travis@$env(SECRET_BUNDLE_IP):~/www
+spawn scp $file travis@198.199.77.172:~/www
 expect "password:"
 send $env(SECRET_BUNDLE_PASS)
 send "\r"
+expect "100%"
 expect eof
