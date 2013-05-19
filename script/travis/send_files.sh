478d5571b753ecc129369ce47c29d37f56a4406d
Bas Veeling <basveeling@gmail.com>
2013-05-19 15:31:50 +0200

    Fixed script

--- script/travis/send_files.sh
+++ script/travis/send_files.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/expect -f
 exp_internal 1
-# set timeout -1
+set timeout 180
 set file [lindex $argv 0]
 spawn scp $file travis@198.199.77.172:~/www
-expect "key fingerprint"
-send "yes\r"
-expect "password:"
-send $env(SECRET_BUNDLE_PASS)
-send "\r"
+expect {
+  "key fingerprint" {send "yes\r"; exp_continue}
+  "assword:" { send $env(SECRET_BUNDLE_PASS); send "\r" }
+}
+
 expect "100%"
 expect eof
+EOF
