a199582b329e1bc3376f6721342e22c6baf81ca0
Bas Veeling <basveeling@gmail.com>
2013-05-19 15:18:05 +0200

    fixed script

--- script/travis/send_files.sh
+++ script/travis/send_files.sh
@@ -3,6 +3,8 @@ exp_internal 1
 # set timeout -1
 set file [lindex $argv 0]
 spawn scp $file travis@198.199.77.172:~/www
+expect "key fingerprint"
+send "yes\r"
 expect "password:"
 send $env(SECRET_BUNDLE_PASS)
 send "\r"
