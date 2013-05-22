b88562a1733caa3ec5040e5f41f4d729808a3e49
Koen van Urk <k.vanurk@student.utwente.nl>
2013-05-22 02:11:35 +0200

    First bits and pieces for the message form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   # Allow regular users some views
-  skip_filter :require_admin, :require_supervisor, :only => [:index, :show, :destroy]
+  skip_filter :require_admin, :require_supervisor, :only => [:index, :show, :destroy, :compose, :deliver]
 
   def index
     if request.xhr?
@@ -19,6 +19,20 @@ class MessagesController < ApplicationController
     @message.destroy
     
     redirect_to messages_path, :notice => "Message deleted."
+  end
+  
+  def compose
+    if request.xhr?
+      render "compose_modal", :layout => false
+    end
+  end
+  
+  def deliver
+    if params.has_key?(:grouptype) && params.has_key?(:groupid) && params.has_key?(:subject) && params.has_key?(:body)
+      render :inline => "Hello"
+    else
+      render :nothing => true
+    end
   end
   
 end
