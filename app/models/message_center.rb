a5665a13ff870effd1df56b4b29b411e4d5d30de
Koen van Urk <k.vanurk@student.utwente.nl>
2013-05-23 14:43:47 +0200

    Adds message button to challenge for supervisor

--- app/models/message_center.rb
+++ app/models/message_center.rb
@@ -1,12 +1,12 @@
 module MessageCenter
   
   def sendMessageToUser(user, subject, body)
-    raise "user must be an instance of User" unless user.is_a? User
+    raise MessageException::ParameterShouldBeUser.new('Group should be an array') unless user.is_a? User
     Message.create(:subject => subject, :body => body, :sender_id => current_user.id, :receiver_id => user.id, :is_read => 0)
   end
   
   def sendMessageToGroup(group, subject, body)
-    raise "group must be an array" unless group.is_a? Array
+    raise MessageException::ParameterShouldBeArray.new('Group should be an array') unless group.is_a? Array
     group.each do |user|
       sendMessageToUser(user, subject, body)
     end
