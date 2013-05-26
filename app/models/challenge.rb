03cab3bd3e435709853af74ff7155e5e78577c75
Bas Veeling <basveeling@gmail.com>
2013-05-26 16:08:26 +0200

    Added fallback images and updated tests with images

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,5 @@
 class Challenge < ActiveRecord::Base
+
   # Supervisors have write access to these fields:
   attr_accessible :title, :description, :end_date, :start_date, :lead, :location, :commitment, :image,  :image_cache, :remove_image
   # But these are protected:
@@ -11,6 +12,9 @@ class Challenge < ActiveRecord::Base
   has_many :participants, :through => :enrollments, :class_name => "User", :conditions => ("unenrolled_at IS NULL")
   has_many :comments, :order => 'updated_at DESC'
 
+  # Carrierwave Image Uploading
+  mount_uploader :image, ChallengeImageUploader
+
   validates :title, :presence => { :message => "One or more fields are missing" }, :if => :pending?
   validates :lead, :presence => { :message => "One or more fields are missing" }, :if => :pending?
   validates :lead, :length => { :in => 40..120 }, :if => :pending?
@@ -18,8 +22,10 @@ class Challenge < ActiveRecord::Base
   validates :commitment, :numericality => {:only_integer => true, :greater_than_or_equal_to => 1, less_than_or_equal_to: 40}
   validates :start_date, :presence => { :message => "One or more fields are missing" }, :if => :pending?
   validates :end_date, :presence => { :message => "One or more fields are missing" }, :if => :pending?
-
   validate :dates, :if => :pending?
+  validates_processing_of :image
+
+
   scope :upcoming, where('start_date > ?', Date.today)
   scope :running, where('end_date > ? AND start_date < ?', Date.today, Date.today)
   scope :upcoming_and_running, where('end_date > ?', Date.today)
@@ -40,8 +46,6 @@ class Challenge < ActiveRecord::Base
     end
   }
 
-  # Carrierwave Image Uploading
-  mount_uploader :image, ChallengeImageUploader
 
   def enroll(user)
     enrollment = enrollments.find_by_participant_id(user)
--- features/step_definitions/addchallenge_steps.rb
+++ features/step_definitions/addchallenge_steps.rb
@@ -1,18 +1,20 @@
-When(/^I fill in title with "(.*?)" and description with "(.*?)" and fill in start_date with "(.*?)" and end_date with "(.*?)" and lead with "(.*?)" and commitment with "(.*?)"$/) do | title, description, start_date, end_date, lead, commitment |
+When(/^I fill in title with "(.*?)" and description with "(.*?)" and fill in start_date with "(.*?)" and end_date with "(.*?)" and lead with "(.*?)" and commitment with "(.*?)" and image with "(.*?)"$/) do | title, description, start_date, end_date, lead, commitment, image |
     fill_in :challenge_title, :with => title
     fill_in :challenge_description, :with => description
     fill_in :challenge_start_date, :with => start_date
     fill_in :challenge_end_date, :with => end_date
     fill_in :challenge_lead, :with => lead
     fill_in :challenge_commitment, :with => commitment
+    if image.present?
+        attach_file :challenge_image, File.expand_path("features/test_assets/images/" + image)
+    end
 end
+
 Then(/^I should see a title "(.*?)" and description "(.*?)" and start_date "(.*?)" and end_date "(.*?)"$/) do |title, description, start_date, end_date|
-  
     page.find_by_id("title").should have_content(title)
     page.find_by_id("description").should have_content(description)
     page.find_by_id("start_date").should have_content(start_date)
     page.find_by_id("end_date").should have_content(end_date)
-    
 end
 
 When(/^I press "(.*?)"$/) do |button|
