1c2164a15871e5c3bdd8310b1d053de8ea23a774
Bram L <bcleenders@gmail.com>
2013-06-04 01:55:03 +0200

    This should fix spec Added first and last name

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -3,8 +3,20 @@ require_relative '../spec_helper'
 describe Follow do
   
   it "should delete all follow relations if an user deletes there account" do
-    user_one = User.create(:email => 'user1@student.utwente.nl', :password => 'blaat123456', :password_confirmation => 'blaat123456')
-    user_two = User.create(:email => 'user2@student.utwente.nl', :password => 'blaat123456', :password_confirmation => 'blaat123456')
+    user_one = User.create(
+      :email => 'user1@student.utwente.nl', 
+      :password => 'blaat123456', 
+      :password_confirmation => 'blaat123456',
+      :first_name => "John"
+      :last_name => "Doe"
+    )
+    user_two = User.create(
+      :email => 'user2@student.utwente.nl', 
+      :password => 'blaat123456', 
+      :password_confirmation => 'blaat123456',
+      :first_name => "Jane"
+      :last_name => "Doe"
+    )
     
     follow = Follow.create(:user_id => user_one.id, :following_id => user_two.id)
     
@@ -13,25 +25,34 @@ describe Follow do
       f = Follow.where(:user_id => user_one_id)
       f.count.should be_zero
     end
-    
   end
   
   it "should delete all following relations if an user deletes there account" do
-     user_one = User.create(:email => 'user1@student.utwente.nl', :password => 'blaat123456', :password_confirmation => 'blaat123456')
-     user_two = User.create(:email => 'user2@student.utwente.nl', :password => 'blaat123456', :password_confirmation => 'blaat123456')
+     user_one = User.create(
+      :email => 'user1@student.utwente.nl', 
+      :password => 'blaat123456', 
+      :password_confirmation => 'blaat123456',
+      :first_name => "John"
+      :last_name => "Doe"
+    )
+    user_two = User.create(
+      :email => 'user2@student.utwente.nl', 
+      :password => 'blaat123456', 
+      :password_confirmation => 'blaat123456',
+      :first_name => "Jane"
+      :last_name => "Doe"
+    )
 
-     #user two follows user one
-     follow = Follow.create(:user_id => user_two.id, :following_id => user_one.id)
+    #user two follows user one
+    follow = Follow.create(:user_id => user_two.id, :following_id => user_one.id)
      
-     user_one_id = user_one.id
+    user_one_id = user_one.id
      
-     #user one is deleted
-     if user_one.destroy
-        #there should be no records of any user following user one
-        f = Follow.where(:following_id => user_one_id)
-        f.count.should be_zero
-     end
-    
+    #user one is deleted
+    if user_one.destroy
+      #there should be no records of any user following user one
+      f = Follow.where(:following_id => user_one_id)
+      f.count.should be_zero
+    end
   end
-  
 end
