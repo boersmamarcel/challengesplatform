7dcef1760162a3ca3f97e1edfee560d6283aa09d
Marcel Boersma <boersma.marcel@gmail.com>
2013-06-06 18:59:50 +0200

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Challengesplatform::Application.routes.draw do
   resources :messages, :only => [:show, :destroy, :index]
 
   namespace :admin do
-    resources :review, :only => [:comment, :decline, :approve, :edit, :index, :show, :update] do
+    resources :review, :only => [:decline, :approve, :edit, :index, :show, :update] do
       post 'decline', :on => :member
       post 'approve', :on => :member
       get 'edit', :on => :member
