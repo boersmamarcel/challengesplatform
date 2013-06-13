04d7b24942d60a7bed9c3575ad5fd42fece806c6
Bram L <bcleenders@gmail.com>
2013-06-13 20:53:00 +0200

    Fixed failing tests

--- app/helpers/supervisor/review_helper.rb
+++ app/helpers/supervisor/review_helper.rb
@@ -3,7 +3,7 @@ module Supervisor::ReviewHelper
 	def submit_resubmit_name(challenge)
 		if challenge.declined?
 			return "Resubmit for Review"
-		elsif challenge.draft?
+		elsif challenge.id.nil? || challenge.draft?
 			return "Submit for Review"
 		end
 	end
