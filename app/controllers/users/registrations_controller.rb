488eb1ec13f38ea48a327a6881671cd23afef89b
Koen van Urk <k.vanurk@student.utwente.nl>
2013-06-14 02:42:50 +0200

    Removes failing code (we don't do email confirmations)
    also adds test for changing password!

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -37,11 +37,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     elsif !params[:user][:password].blank?
           if resource.update_with_password(params[:user])
             prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-            if is_navigational_format?
-              flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-                :update_needs_confirmation : :updated
-              set_flash_message :notice, flash_key
-            end
             sign_in resource_name, resource, :bypass => true
             respond_with resource, :location => after_update_path_for(resource)
         else
