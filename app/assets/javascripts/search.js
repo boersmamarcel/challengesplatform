2b919e04be68175fb06f01a0ab40ae02eafedf72
Bram L <bcleenders@gmail.com>
2013-07-03 01:33:44 +0200

    Search results to List.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,8 +2,10 @@ if(document.getElementById('page_identifier').value == 'search.index') {
   enableList();
 
   var options = {
-      valueNames: [ 'category', 'name' ]
+      valueNames: [ 'id', 'name' ]
   };
+
+  var userList = new List('search-list', options);
 
   /*$.getJSON(data_resource, function(data ) {
     var userList = new List('search-list', options, data);
@@ -27,19 +29,18 @@ if(document.getElementById('page_identifier').value == 'search.index') {
       var req = $.post(query_path, {q: query}, "json");
 
       req.done(function(data) {
-        var results = "";
-
-        if(data.length >= 1) {
-          results = data[0].challenge.title + " (" + data[0].challenge.id + ")";
-          for(var i = 1; i < data.length; i++) {
-            results += ", " + data[i].challenge.title + " (" + data[i].challenge.id + ")";;
+        for(var i = 0; i < data.length; i++) {
+          if(data[i].challenge != undefined) {
+            $.extend(data[i].challenge, {type: "challenge", onel: "Oneliner..."});
+            userList.add(data[i].challenge);
+          }
+          else {
+            console.log("data.challenge not defined, object; " + data[i]);
           }
         }
-        else {
-          results = "No results found";
-        }
 
-        $('#results_id').html(results);
+        if(data.length < 1)
+          $('#results_id').html("No results found");
       })
     }
   })
